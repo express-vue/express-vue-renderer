94f6616cc9dbf5fa8a26513ceca824b68030d201
Daniel Cherubini <daniel@cherubini.casa>
2017-07-06 21:35:04 +0200

    nearly ready

--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@
 const {Defaults} = require('./models');
 const Utils = require('./utils');
 const Renderer = require('./renderer');
+const renderer = require('vue-server-renderer').createRenderer();
+
 
 const regexes = {
     appRegex   : /{{{\s*app\s*}}}/igm,
     scriptRegex: /{{{\s*script\s*}}}/igm,
     headRegex  : /<\/head>/igm
 };
+
+function renderToString(rendered: Object, defaults: Defaults): Promise<string> {
+    return new Promise((resolve, reject) => {
+        renderer.renderToString(rendered.app, function (error, renderedHtml) {
+            if (error) {
+                reject(error);
+            } else {
+                let html = '';
+                let head = '';
+                html = rendered.layout.template.replace(regexes.appRegex, `<div id="app">${renderedHtml}</div>`);
+                html = html.replace(regexes.scriptRegex, rendered.scriptString);
+                head = Utils.headUtil(defaults.options.vue, rendered.layout.style);
+                html = html.replace(regexes.headRegex, head);
+                resolve(html);
+            }
+        });
+    });
+}
 
 function expressVueRenderer(componentPath: string, options: Object): Promise<Object> {
     const defaults = new Defaults(options);
@@ -22,7 +42,11 @@ function expressVueRenderer(componentPath: string, options: Object): Promise<Obj
                         if (!rendered) {
                             reject(Renderer.renderError('Renderer Error'));
                         } else {
-                            resolve(rendered);
+                            const app = {
+                                rendered: rendered,
+                                defaults: defaults
+                            };
+                            resolve(app);
                         }
                     })
                     .catch(function(error) {
--- tests/expressVueRenderer.test.js
+++ tests/expressVueRenderer.test.js
@@ -1,22 +1,12 @@
 const expressVueRenderer = require('../lib');
-const Utils = require('../lib/utils')
-const renderer = require('vue-server-renderer').createRenderer();
-const regexes = expressVueRenderer.regexes;
 
-expressVueRenderer.expressVueRenderer().then(rendered => {
-    renderer.renderToString(rendered.app, function (error, renderedHtml) {
-        if (error) {
-            console.error(error);
-        } else {
-            let html = '';
-            let head = '';
-            html = rendered.layout.template.replace(regexes.appRegex, `<div id="app">${renderedHtml}</div>`);
-            html = html.replace(regexes.scriptRegex, rendered.scriptString);
-            head = Utils.headUtil(defaults.options.vue, rendered.layout.style);
-            html = html.replace(regexes.headRegex, head);
-            console.log(html);
-        }
+
+expressVueRenderer.expressVueRenderer().then(app => {
+    expressVueRenderer.renderToString(app).then(string => {
+        console.error(string);
+    }).catch(error => {
+        console.error(error);
     });
 }).catch(error => {
     console.error(error);
-})
+});
