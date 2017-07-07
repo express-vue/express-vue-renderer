9c6db2b0ebe3554d0d9105f3568853fd66acb7ec
Nikolaos Fytros <nikolaos.fytros@tidal.com>
2017-07-07 17:00:55 +0200

    cache the component files

--- src/parser/index.js
+++ src/parser/index.js
@@ -12,11 +12,16 @@ const styleRegex = /(<style.*?>)([\s\S]*?)(<\/style>)/gm;
 
 function componentParser(templatePath: string, defaults: Object, type: string): Promise < Object > {
     return new Promise(function (resolve, reject) {
-        defaults.cache.get(templatePath, (error, cachedComponent) => {
+        defaults.cache.get(templatePath, (error, cachedComponentContent) => {
             if (error) {
                 reject(new Error(error));
-            } else if (cachedComponent) {
-                resolve(cachedComponent);
+            } else if (cachedComponentContent) {
+                const componentObject = parseContent(cachedComponentContent, templatePath, defaults, type);
+                if (typeof componentObject === Error) {
+                    reject(componentObject);
+                } else {
+                    resolve(componentObject);
+                }
             } else {
                 fs.readFile(templatePath, 'utf-8', function (err, content) {
                     if (err) {
@@ -24,37 +29,17 @@ function componentParser(templatePath: string, defaults: Object, type: string): 
                         console.error(new Error(error));
                         reject(error);
                     } else {
-                        const body = htmlParser(content, htmlRegex, true);
-                        content = content.replace(htmlRegex, '');
-
-                        const script = scriptParser(content, defaults, type, scriptRegex);
-                        const templateArray = templatePath.split('/');
-
-                        const style = styleParser(content, styleRegex);
-                        content = content.replace(styleRegex, '');
-
-                        if (templateArray.length === 0) {
-                            let error = `I had an error processing component templates. in this file \n${templatePath}`;
-                            console.error(new Error(error));
-                            reject(error);
-                        }
-
-                        let templateName = templateArray[templateArray.length - 1].replace('.vue', '');
-                        let componentScript = script || {};
-                        componentScript.template = body;
-
-                        const componentObject = {
-                            type: type,
-                            style: style,
-                            name: camelCase(templateName),
-                            script: componentScript
-                        };
                         // set the cache for the component
-                        defaults.cache.set(templatePath, componentObject, error => {
+                        defaults.cache.set(templatePath, content, error => {
                             if (error) {
                                 reject(new Error(error));
                             } else {
-                                resolve(componentObject);
+                                const componentObject = parseContent(content, templatePath, defaults, type);
+                                if (typeof componentObject === Error) {
+                                    reject(componentObject);
+                                } else {
+                                    resolve(componentObject);
+                                }
                             }
                         });
                     }
@@ -63,6 +48,35 @@ function componentParser(templatePath: string, defaults: Object, type: string): 
         });
 
     });
+}
+
+function parseContent(content: string, templatePath: string, defaults: Object, type: string): Object {
+    const body = htmlParser(content, htmlRegex, true);
+    content = content.replace(htmlRegex, '');
+
+    const script = scriptParser(content, defaults, type, scriptRegex);
+    const templateArray = templatePath.split('/');
+
+    const style = styleParser(content, styleRegex);
+    content = content.replace(styleRegex, '');
+
+    if (templateArray.length === 0) {
+        let error = `I had an error processing component templates. in this file \n${templatePath}`;
+        console.error(new Error(error));
+        return error;
+    }
+
+    let templateName = templateArray[templateArray.length - 1].replace('.vue', '');
+    let componentScript = script || {};
+    componentScript.template = body;
+
+    const componentObject = {
+        type: type,
+        style: style,
+        name: camelCase(templateName),
+        script: componentScript
+    };
+    return componentObject;
 }
 
 module.exports.componentParser = componentParser;
