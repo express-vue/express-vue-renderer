d7eebfabcde1735ac787b96012ca892072c7d685
Daniel Cherubini <daniel@cherubini.casa>
2017-08-01 22:57:32 +0200

    requires components from vuefiles

--- src/utils/componentArray.js
+++ src/utils/componentArray.js
@@ -8,27 +8,11 @@ let types = new Models.Types();
 function setupComponentArray(componentPath: string, defaults: Models.Defaults) {
     return new Promise((resolve, reject) => {
         let array = [];
-        let pathPromiseArray = [];
         const vueFile = componentPath.includes('.vue') ? componentPath : componentPath + '.vue';
         PathUtils.getCorrectPathForFile(vueFile, defaults.viewsPath, 'view', defaults)
             .then(path => {
                 array.push(Parser.componentParser(path.path, defaults, types.COMPONENT));
-                if (defaults.vue && defaults.vue.components) {
-                    for (let component of defaults.vue.components) {
-                        const componentFile = component.includes('.vue') ? component : component + '.vue';
-                        pathPromiseArray.push(PathUtils.getCorrectPathForFile(componentFile, defaults.componentsPath ,'component', defaults));
-                    }
-                }
-                Promise.all(pathPromiseArray)
-                    .then(pathObjArray => {
-                        for (var pathObj of pathObjArray) {
-                            array.push(Parser.componentParser(pathObj.path, defaults, types.SUBCOMPONENT));
-                        }
-                        resolve(array);
-                    })
-                    .catch(error => {
-                        reject(error);
-                    });
+                resolve(array);
             })
             .catch(error => {
                 reject(error);
--- src/utils/require.js
+++ src/utils/require.js
@@ -1,31 +1,82 @@
 // @flow
-var Module = require('module');
-var path = require('path');
+const Module = require('module');
+const path = require('path');
+const Utils = require('./index');
+const Renderer = require('../renderer');
+const Models = require('../models');
 
 class Options {
+    regex: RegExp;
     appendPaths: string[];
     prependPaths: string[];
     constructor(optsObj: Object) {
+        this.regex = /(require\(')([\w/.\d]*\.vue)('\))/igm;
         this.appendPaths = optsObj.appendPaths || [];
         this.prependPaths = optsObj.prependPaths || [];
     }
 }
 
-function requireFromString(code: string, filename: string = '', optsObj: Object = {}) {
-    const options = new Options(optsObj);
+function getVueObject(componentPath: string, rootPath: string): Promise < Object > {
+    const GlobalOptions = new Models.Defaults({
+        viewsPath: rootPath
+    });
+    return new Promise((resolve, reject) => {
+        Utils.setupComponentArray(componentPath, GlobalOptions)
+            .then(promiseArray => {
+                Promise.all(promiseArray)
+                    .then((components) => {
+                        const rendered = Renderer.renderHtmlUtil(components);
+                        if (!rendered) {
+                            reject(new Error('Renderer Error'));
+                        } else {
+                            resolve(rendered);
+                        }
+                    }).catch((error) => {
+                        reject(error);
+                    });
+            }).catch(error => {
+                reject(error);
+            });
+    });
+}
 
-    if (typeof code !== 'string') {
-        throw new Error('code must be a string, not ' + typeof code);
-    }
 
-    var paths = Module._nodeModulePaths(path.dirname(filename));
+function requireFromString(code: string, filename: string = '', optsObj: Object = {}): Promise < Object > {
+    return new Promise((resolve, reject) => {
+        const options = new Options(optsObj);
 
-    var m = new Module(filename, module.parent);
-    m.filename = filename;
-    m.paths = [].concat(options.prependPaths).concat(paths).concat(options.appendPaths);
-    m._compile(code, filename);
+        if (typeof code !== 'string') {
+            throw new Error('code must be a string, not ' + typeof code);
+        }
 
-    return m;
+        var paths = Module._nodeModulePaths(path.dirname(filename));
+
+        var m = new Module(filename, module.parent);
+        m.filename = filename;
+        m.paths = [].concat(options.prependPaths).concat(paths).concat(options.appendPaths);
+        try {
+            m._compile(code, filename);
+            resolve(m.exports.default);
+        } catch (error) {
+            if (error.message === 'Unexpected token <') {
+                let vueComponentFileArray = options.regex.exec(code);
+                if (vueComponentFileArray && vueComponentFileArray.length > 0) {
+                    //found a vue component
+                    const vueComponentFile = vueComponentFileArray[2] ? vueComponentFileArray[2] : null;
+                    if (vueComponentFile) {
+                        getVueObject(vueComponentFile, m.paths[0])
+                            .then(rendered => {
+                                const newCode = code.replace(options.regex, rendered.scriptStringRaw);
+                                m._compile(newCode, filename);
+                                resolve(m.exports.default);
+                            })
+                            .catch(error => reject(error));
+                    }
+                }
+            }
+        }
+
+    });
 }
 
 module.exports = requireFromString;
