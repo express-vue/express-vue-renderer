d5ca52674f10c58eccf133a735fef0c2e419e5d7
Daniel Cherubini <daniel@cherubini.casa>
2017-08-07 15:47:55 +0200

    should do the trick

--- example/components/uuid.vue
+++ example/components/uuid.vue
@@ -1,14 +1,19 @@
 <template>
     <div>
+        <inner></inner>
         <h2>Uuid: {{uuid ? uuid : 'no uuid'}}</h2>
     </div>
 </template>
 
 <script>
+import inner from '../components/inner.vue';
 export default {
     props: ['uuid'],
     data: function () {
         return {}
+    },
+    components: {
+        inner: inner
     }
 }
 </script>
