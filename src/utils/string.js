81a0ab66ca626bff8e6302f09c7254c43f89a242
Daniel Cherubini <daniel@cherubini.casa>
2017-09-29 15:33:11 +0200

    prop object, and source string contents

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,69 +1,104 @@
 // @flow
 const xss = require('xss');
 
+class PropClass {
+    type: any;
+    required: boolean;
+    default: any;
+    constructor(prop: any) {
+        prop.required ? this.required = prop.required : null;
+        prop.default ? this.default = prop.default : null;
+    }
+}
 
 function routesToString(routes: Object[]): string {
-    let string = '';
-    routes.forEach(script => string += scriptToString(script) + ',');
-    return `[${string}]`;
+    let routeString = '';
+    routes.forEach(script => routeString += scriptToString(script) + ',');
+    return `[${routeString}]`.replace(new RegExp(/,}/, 'g'), '}').replace(new RegExp(/,]/, 'g'), ']');
 }
 
 function routeComponentsToString(script: Object): string {
-    let string = '';
+    let componentString = '';
     for (let member in script) {
-        string += member + ': __' + script[member] + ',';
+        componentString += member + ': __' + script[member] + ',';
     }
-    return `{${string}}`;
+    return `{${componentString}}`;
 }
 
 function mixinsToString(mixins: Array < Object > ): string {
-    var string = '';
+    var mixinString = '';
     for (var mixin of mixins) {
-        string += `${scriptToString(mixin)},`;
+        mixinString += `${scriptToString(mixin)},`;
     }
-    return string;
+    return mixinString;
 }
 
-function scriptToString(script: Object): string {
-    let string = '';
+function propsToString(props: Object): string {
+    let propString = '';
+    if (props[Object.keys(props)[0]].type === null) {
+        var propsArray = Object.keys(props);
+        propString = xss(JSON.stringify(propsArray));
+    } else {
+        let tempProp = {};
+        for (var prop in props) {
+            if (props.hasOwnProperty(prop)) {
+                var element = new PropClass(props[prop]);
+                tempProp[prop] = element;
+            }
+        }
+        propString = scriptToString(tempProp);
+    }
+    return propString;
+}
+
+function scriptToString(script: Object | any): string {
+    let scriptString = '';
     for (let member in script) {
         switch (typeof script[member]) {
             case 'function':
                 if (member === 'data') {
                     const dataObj = xss(JSON.stringify(script[member]()));
-                    string += `${member}: function(){return ${dataObj}},`;
+                    scriptString += `${member}: function(){return ${dataObj}},`;
                 } else {
-                    string += member + ': ' + String(script[member]) + ',';
+                    scriptString += member + ': ' + String(script[member]) + ',';
                 }
                 break;
             case 'object':
                 if (member === 'data') {
-                    string += member + ': ' + xss(JSON.stringify(script[member])) + ',';
+                    scriptString += member + ': ' + xss(JSON.stringify(script[member])) + ',';
                 } else if (member === 'routes' || member === 'children') {
-                    string += member + ': ' + routesToString(script[member]) + ',';
+                    scriptString += member + ': ' + routesToString(script[member]) + ',';
                 } else if (member === 'components' && script['path'] !== undefined) { // Checks if 'components' is in a route object
-                    string += member + ': ' + routeComponentsToString(script[member]) + ',';
+                    scriptString += member + ': ' + routeComponentsToString(script[member]) + ',';
                 } else if (member === 'mixins') {
-                    string += member + ': [' + mixinsToString(script[member]) + '],';
+                    scriptString += member + ': [' + mixinsToString(script[member]) + '],';
                 } else if (script[member].constructor === Array) {
-                    string += member + ': ' + xss(JSON.stringify(script[member])) + ',';
+                    scriptString += member + ': ' + xss(JSON.stringify(script[member])) + ',';
                 } else if (member === 'props') {
-                    const propsArray = Object.keys(script[member]);
-                    string += member + ': ' + xss(JSON.stringify(propsArray)) + ',';
+                    if (script[member][Object.keys(script[member])[0]].type === null) {
+                        var propsArray = Object.keys(script[member]);
+                        scriptString += member + ': ' + xss(JSON.stringify(propsArray)) + ',';
+                    } else {
+                        // scriptString += member + ': ' + scriptToString(script[member]) + ',';
+                        const propsString = propsToString(script[member]);
+                        scriptString += `${member}: ${propsString},`;
+                    }
+
                 } else {
-                    string += member + ': ' + scriptToString(script[member]) + ',';
+                    scriptString += member + ': ' + scriptToString(script[member]) + ',';
                 }
                 break;
             default:
                 if (member === 'component' && script['path'] !== undefined) { // Checks if 'component' is in a route object
-                    string += member + ': __' + script[member] + ',';
+                    scriptString += member + ': __' + script[member] + ',';
                 } else {
-                    string += member + ': ' + JSON.stringify(script[member]) + ',';
+                    scriptString += member + ': ' + JSON.stringify(script[member]) + ',';
                 }
                 break;
         }
     }
-    return `{${string}}`;
+    let finalScriptString = `{${scriptString}}`.replace(new RegExp(/,}/, 'g'), '}').replace(new RegExp(/,]/, 'g'), ']');
+    return finalScriptString;
 }
 
 module.exports.scriptToString = scriptToString;
